b5f701f6d827fd5f817100a875f3cf9a4a61ba30
Prochakovsky Alexsandr <prochakovsky@gmail.com>
2019-08-03 15:53:53 +0300

    Add abstract class for all interfaces

---
+++ http_tag_counter/http_tag_counter.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod, ABCMeta
+
+class HttpTagCounter(ABC):
+    """
+    Virtual class for Storage interface
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def process(self, data: str) -> dict:
+        """ method to calculate tag count in provided string (http content)
+
+        Parameters
+        ----------
+        data : str
+            Http raw data represented in string
+        """
